6a969acd773b14426152ae19fa56be05155aa282
StreamAzure <1905105036@qq.com>
2023-07-17 02:55:22 +0800

    实现查询数据库并显示至网页

--- service/service_edu/src/main/java/com/stream/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/stream/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,14 @@
 package com.stream.eduservice.controller;
+import java.util.List;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.stream.eduservice.service.EduTeacherService;
+import com.stream.eduservice.entity.EduTeacher;
 
 /**
  * <p>
@@ -14,8 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2023-07-17
  */
 @RestController
-@RequestMapping("/eduservice/edu-teacher")
+@RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
 
+    @Autowired
+    private EduTeacherService eduTeacherService;
+
+    // 查询讲师表所有数据
+    @GetMapping("findAll")
+    public List<EduTeacher> list(){
+        return eduTeacherService.list(null);
+    }
 }
 
